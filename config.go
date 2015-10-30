98c593d0aad4710d873d941be800887702162482
nagae-memooff <169192@gmail.com>
2015-10-30 13:10:17 +0800

    增加获取布尔值、整型、浮点型的方法

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 	//   "fmt"
 )
@@ -53,7 +54,7 @@ func Parse(filename string) (err error) {
 }
 
 //调试用，返回conf对象的指针，一般情况下最好不要使用
-func GetAll() *map[string]string {
+func GetModel() *map[string]string {
 	return &conf
 }
 
@@ -78,12 +79,41 @@ func GetMulti(keys ...string) (values []interface{}) {
 }
 
 // 以可变参数的形式获取多个键值，并按顺序返回[]string类型的切片。
-func GetMultiValues(keys ...string) (values []string) {
+func GetMultiStrings(keys ...string) (values []string) {
 	for _, key := range keys {
 		values = append(values, conf[key])
 	}
 	return
 }
+
+//TODO:实现根据键值获取指定类型的值
+func GetBool(key string) (value bool) {
+	if conf[key] == "true" {
+		value = true
+	} else {
+		value = false
+	}
+	return
+}
+
+func GetInt(key string) int {
+	value, err := strconv.Atoi(conf[key])
+	if err != nil {
+		value = 0
+	}
+	return value
+}
+
+func GetFloat64(key string) float64 {
+	value, err := strconv.ParseFloat(conf[key], 64)
+	if err != nil {
+		value = 0.0
+	}
+	return value
+}
+
+//func GetArray(key string) {
+//}
 
 func Default(key, value string) {
 	if Get(key) == "" {
